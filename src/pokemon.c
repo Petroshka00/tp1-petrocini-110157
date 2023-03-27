100d8eb0a8bb33e47fdca2b2424f750f4831fc9b
Petroshka00 <petrocinitomas@gmail.com>
2023-03-27 17:52:55 -0300

    Termine la funcion de comparar pokemones y agregue dos funciones auxiliares a esta

--- src/pokemon.c
+++ src/pokemon.c
@@ -13,9 +13,41 @@ pokemon_t *pokemon_copiar(pokemon_t *poke)
 	return NULL;
 }
 
+bool comparar_var_size(size_t var1, size_t var2)
+{
+	if(var1 == var2){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool comparar_var_char(char var1, char var2)
+{
+	if(strcmp(var1, var2) == 0){
+		return true;
+	} else {
+		return false;
+	}
+}
+
 bool pokemon_son_iguales(pokemon_t *pokemon1, pokemon_t *pokemon2)
 {
-	return true;
+	int aux = 0;
+	if(comparar_var_size(pokemon1->id, pokemon2->id))
+		aux++;
+	if(comparar_var_size(pokemon1->salud, pokemon2->salud))
+		aux++;
+	if(comparar_var_char(pokemon1->nombre, pokemon2->nombre))
+		aux++;
+	if(comparar_var_char(pokemon1->nombre_entrenador, pokemon2->nombre_entrenador))
+		aux++;
+
+	if(aux == 4){
+		return true;
+	} else {
+		return false;
+	}
 }
 
 char *pokemon_nombre(pokemon_t *pokemon)
