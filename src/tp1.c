004b2a892416b51372ef68e3812fdbb7cc80a8dc
Petroshka00 <petrocinitomas@gmail.com>
2023-03-29 19:06:10 -0300

    Cambios muy variados aunque no finales y formato

--- src/tp1.c
+++ src/tp1.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LECTURA "%zu,%[^,],%zu,%[^,]"
+#define LECTURA_ARC "%s"
 
 struct _hospital_pkm_t {
 	pokemon_t **pokemones;
@@ -16,19 +16,25 @@ struct _hospital_pkm_t {
 
 hospital_t *hospital_crear_desde_archivo(const char *nombre_archivo)
 {
-	FILE *archivo = fopen(nombre_archivo, "r");
+	FILE *archivo = malloc(sizeof(FILE));
+	archivo = fopen(nombre_archivo, "r");
 	if (archivo == NULL) {
 		fclose(archivo);
 		return NULL;
 	}
-	int leidos = 0;
+
 	hospital_t *hospital_nuevo = malloc(sizeof(hospital_t));
+	int leidos = 0;
+	char string_para_crear_pkm[1000];
 	hospital_nuevo->cantidad_pokemon = 0;
 	do {
-		leidos = fscanf(archivo, LECTURA, hospital_nuevo->pokemones[hospital_nuevo->cantidad_pokemon]);
+		leidos = fscanf(archivo, LECTURA_ARC, string_para_crear_pkm);
+		hospital_nuevo->pokemones[hospital_nuevo->cantidad_pokemon] =
+			pokemon_crear_desde_string(string_para_crear_pkm);
 		hospital_nuevo->cantidad_pokemon += 1;
-	} while (leidos == 4);
-	return NULL;
+	} while (leidos == 1);
+	fclose(archivo);
+	return hospital_nuevo;
 }
 
 size_t hospital_cantidad_pokemones(hospital_t *hospital)
@@ -40,7 +46,12 @@ size_t hospital_a_cada_pokemon(hospital_t *hospital,
 			       bool (*funcion)(pokemon_t *p, void *aux),
 			       void *aux)
 {
-	return 0;
+	int i = 0;
+	do {
+		funcion(hospital->pokemones[i], aux);
+		i++;
+	} while (funcion(hospital->pokemones[i], aux));
+	return (size_t)i;
 }
 
 int hospital_aceptar_emergencias(hospital_t *hospital,
@@ -52,7 +63,7 @@ int hospital_aceptar_emergencias(hospital_t *hospital,
 
 pokemon_t *hospital_obtener_pokemon(hospital_t *hospital, size_t prioridad)
 {
-	if(hospital == NULL){
+	if (hospital == NULL) {
 		return NULL;
 	}
 	return (hospital->pokemones[prioridad]);
