82dd4e8b203a86921b8f668b1388f668bfa4649b
Petroshka00 <petrocinitomas@gmail.com>
2023-03-30 14:21:07 -0300

    Formateo

--- src/tp1.c
+++ src/tp1.c
@@ -14,14 +14,19 @@ struct _hospital_pkm_t {
 	size_t cantidad_entrenadores;
 };
 
-void hospital_ordenar(hospital_t *hospital_a_ordenar){
-	for(size_t i = 0; i < hospital_a_ordenar->cantidad_pokemon-1; i++){
-		for(size_t j = 0; j < hospital_a_ordenar->cantidad_pokemon - i - 1; j++){
-			if(pokemon_salud(hospital_a_ordenar->pokemones[j+1]) < pokemon_salud(hospital_a_ordenar->pokemones[j])){
+void hospital_ordenar(hospital_t *hospital_a_ordenar)
+{
+	for (size_t i = 0; i < hospital_a_ordenar->cantidad_pokemon - 1; i++) {
+		for (size_t j = 0;
+		     j < hospital_a_ordenar->cantidad_pokemon - i - 1; j++) {
+			if (pokemon_salud(
+				    hospital_a_ordenar->pokemones[j + 1]) <
+			    pokemon_salud(hospital_a_ordenar->pokemones[j])) {
 				void *aux;
 				aux = hospital_a_ordenar->pokemones[j];
-				hospital_a_ordenar->pokemones[j] = hospital_a_ordenar->pokemones[j+1];
-				hospital_a_ordenar->pokemones[j+1] = aux;
+				hospital_a_ordenar->pokemones[j] =
+					hospital_a_ordenar->pokemones[j + 1];
+				hospital_a_ordenar->pokemones[j + 1] = aux;
 			}
 		}
 	}
@@ -43,20 +48,21 @@ hospital_t *hospital_crear_desde_archivo(const char *nombre_archivo)
 	}
 
 	char *string_para_crear_pkm = malloc(40 * sizeof(char));
-	if(string_para_crear_pkm == NULL){
+	if (string_para_crear_pkm == NULL) {
 		return NULL;
 	}
 	int leidos = fscanf(archivo, LECTURA_ARC, string_para_crear_pkm);
 
-	if(leidos != 1){
+	if (leidos != 1) {
 		return NULL;
 	}
 
-	while(leidos == 1){
-		if(string_para_crear_pkm == NULL){
+	while (leidos == 1) {
+		if (string_para_crear_pkm == NULL) {
 			return NULL;
 		}
-		hospital_nuevo->pokemones[hospital_nuevo->cantidad_pokemon] = pokemon_crear_desde_string(string_para_crear_pkm);
+		hospital_nuevo->pokemones[hospital_nuevo->cantidad_pokemon] =
+			pokemon_crear_desde_string(string_para_crear_pkm);
 		hospital_nuevo->cantidad_pokemon += 1;
 		hospital_nuevo->cantidad_entrenadores += 1;
 		leidos = fscanf(archivo, LECTURA_ARC, string_para_crear_pkm);
@@ -94,7 +100,7 @@ int hospital_aceptar_emergencias(hospital_t *hospital,
 pokemon_t *hospital_obtener_pokemon(hospital_t *hospital, size_t prioridad)
 {
 	hospital_ordenar(hospital);
-	if(hospital->cantidad_pokemon < prioridad){
+	if (hospital->cantidad_pokemon < prioridad) {
 		return NULL;
 	} else {
 		return (hospital->pokemones[prioridad]);
